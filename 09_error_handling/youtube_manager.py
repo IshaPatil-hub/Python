18eb0ba8f760249c3a3c82a7f7cd979ab722b409
Isha Patil <ishapatil093@gmail.com>
2026-03-16 16:42:24 +0530

    Added API handling scripts

---
+++ 09_error_handling/youtube_manager.py
@@ -0,0 +1,40 @@
+
+def list_all_videos(videos):
+    pass
+
+def add_videos(videos):
+    pass
+
+def update_videos(videos):
+    pass
+
+def delete_videos(videos):
+    pass
+
+
+def main():
+    videos = []
+    while True:
+        print("\n Youtube Manager | choose an option ")
+        print("1. List all youtube videos ")
+        print("2. Add a youtube video ")
+        print("3. Update a youtube video details ")
+        print("4. Delete a youtube video ")
+        print("5. Exit the app ")
+        choice = input("Enter your choice")
+
+        match choice:
+            case '1':
+                list_all_videos(videos)
+            case '2':
+                add_videos(videos)
+            case '3':
+                update_videos(videos)
+            case '4':
+                delete_videos(videos)
+            case '5':
+                break
+            case _:
+                print("Invalid choice")
+
+main()
